688981ba885dfc198f339ba2908cde2b840bcf1c
vatsalsaglani <saglanivatsal@gmail.com>
2022-03-16 21:55:32 +0530

    main train file

---
+++ scripts/train.py
@@ -0,0 +1,63 @@
+from train_pipeline import trainer
+from constants import TRAIN_CONSTANTS
+
+from rich.table import Column, Table
+from rich import box
+from rich.console import Console
+
+console = Console(record=True)
+
+training_logger = Table(
+    Column("Epoch", justify="center"),
+    Column("Loss", justify="center"),
+    Column("Accuracy", justify="center"),
+    title="Training Status",
+    pad_edge=False,
+    box=box.ASCII,
+)
+
+valid_loggger = Table(
+    Column("Epoch", justify="center"),
+    Column("Loss", justify="center"),
+    Column("Accuracy", justify="center"),
+    title="Validation Status",
+    pad_edge=False,
+    box=box.ASCII,
+)
+
+loggers = dict(CONSOLE=console,
+               TRAIN_LOGGER=training_logger,
+               VALID_LOGGER=valid_loggger)
+
+model_params = dict(
+    SEED=3007,
+    VOCAB_SIZE=9725,
+    heads=4,
+    layers=6,
+    emb_dim=512,
+    pad_id=TRAIN_CONSTANTS.PAD,
+    history=TRAIN_CONSTANTS.HISTORY,
+    trained=None,
+    LEARNING_RATE=1e-4,
+    EPOCHS=100,
+    SAVE_NAME="bert4rec.pt",
+    SAVE_STATE_DICT_NAME="bert4rec-state-dict.pth",
+)
+
+data_params = dict(path="../data/ratings_mapped.csv",
+                   group_by_col="userId",
+                   data_col="movieId_mapped",
+                   train_history=TRAIN_CONSTANTS.HISTORY,
+                   valid_history=5,
+                   padding_mode="right",
+                   MASK=TRAIN_CONSTANTS.MASK,
+                   LOADERS=dict(TRAIN=dict(batch_size=8,
+                                           shuffle=True,
+                                           num_workers=0),
+                                VALID=dict(batch_size=4,
+                                           shuffle=False,
+                                           num_workers=0)))
+
+output_dir = "../models/bert4rec-itr-1"
+
+trainer(data_params, model_params, loggers, output_dir)
