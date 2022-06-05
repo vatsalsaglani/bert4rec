bb9c2aff0e7cd32bc417a2da749625d93b76d668
vatsalsaglani <saglanivatsal@gmail.com>
2022-06-05 16:22:12 +0530

    number of channels to have item embedding

--- scripts/bert4rec_model.py
+++ scripts/bert4rec_model.py
@@ -14,7 +14,8 @@ class RecommendationModel(nn.Module):
                  layers=6,
                  emb_dim=512,
                  pad_id=0,
-                 num_pos=120):
+                 num_pos=128,
+                 num_channels=128):
         """Recommendation model initializer
 
         Args:
@@ -24,6 +25,7 @@ class RecommendationModel(nn.Module):
             emb_dim (int, optional): Embedding Dimension. Defaults to 512.
             pad_id (int, optional): Token used to pad tensors. Defaults to 0.
             num_pos (int, optional): Positional Embedding, fixed sequence. Defaults to 120.
+            num_channels (int, optional): Channels for Item Embeddings
         """
         super().__init__()
         self.emb_dim = emb_dim
@@ -31,19 +33,21 @@ class RecommendationModel(nn.Module):
         self.num_pos = num_pos
         self.vocab_size = vocab_size
         print(f'NUM POS: ', num_pos)
+        self.item_embeddings = nn.Embedding(self.vocab_size,
+                                            embedding_dim=num_channels)
         self.encoder = Encoder(
-            source_vocab_size=vocab_size,
-            emb_dim=emb_dim,
+            source_vocab_size=num_channels,
+            emb_dim=num_channels,
             layers=layers,
             heads=heads,
-            dim_key=emb_dim,
-            dim_value=emb_dim,
-            dim_model=emb_dim,
-            dim_inner=emb_dim * 4,
+            dim_key=num_channels,
+            dim_value=num_channels,
+            dim_model=num_channels,
+            dim_inner=num_channels * 4,
             pad_id=pad_id,
             num_pos=num_pos,
         )
-        self.lin_op = nn.Linear(emb_dim, self.vocab_size)
+        self.lin_op = nn.Linear(num_channels, self.vocab_size)
 
     def forward(self, batch):
         """Returns predictions for a given sequence of tokens/items
@@ -54,9 +58,21 @@ class RecommendationModel(nn.Module):
         Returns:
             torch.Tensor: Prediction for masked items
         """
-        batch_size = batch.size(0)
+        # batch_size = batch.size(0)
+        # print(f'BATCH: {batch.size()}')
+        src_items = self.item_embeddings(batch)
+        # print(f'ITEMS: {src_items.size()}')
+        batch_size, in_sequence_len = src_items.size(0), src_items.size(1)
+        batch = T.arange(0, in_sequence_len,
+                         device=src_items.device).unsqueeze(0).repeat(
+                             batch_size, 1)
+        # print(f'REPEAT BATCH: {batch.size()}')
         op = self.encoder(batch, None)
-        op = op.view(-1, op.size(2))
+        # print(f'ENCODED BATCH: {op.size()}')
+        # op = op.view(-1, op.size(2))
+        op = op.permute(1, 0, 2)
+        # print(f'PERMUTED BATCH: {op.size()}')
         op = self.lin_op(op)
-        op = op.view(batch_size, int(op.size(0) / batch_size), -1)
+        # print(f'AFTER LINEAR BATCH: {op.size()}')
+        # op = op.view(batch_size, int(op.size(0) / batch_size), -1)
         return op
