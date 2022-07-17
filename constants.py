e80d62a96074e1ee6205f2fb10fa935e5e583575
vatsalsaglani <saglanivatsal@gmail.com>
2022-07-17 19:43:28 +0530

    added extra variables to constants

--- constants.py
+++ constants.py
@@ -4,3 +4,8 @@ class TRAIN_CONSTANTS:
     PAD = 0
     MASK = 1
     HISTORY = 120
+    VOCAB_SIZE = 59049
+    HEADS = 4
+    LAYERS = 6
+    EMB_DIM = 256
+    MODEL_PATH = "../models/rec-transformer-model-2/model_files/bert4rec-state-dict.pth"
